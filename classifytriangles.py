e07e46d7a5ffaa9f1c0613a6b5f473371b5f03ab
A-very-Cunning-ham <ave210knife@gmail.com>
2021-02-05 20:40:42 -0500

    Update classifytriangles.py

--- classifytriangles.py
+++ classifytriangles.py
@@ -3,12 +3,15 @@ from math import sqrt
 
 
 def classify_triangle(a, b, c):
-    """ based on 3 side lengths, return the triangle type: scalene, isosceles, or equilateral, and if  it is a right triangle"""
+    """ 
+    Based on 3 side lengths, return the triangle properties: scalene, isosceles, or equilateral, and if it is a right triangle.
+    Triangles are invalid if the summed length of their two shortest sides is not longer than the longest side
+    """
 
     a, b, c = sorted([a, b, c])
 
     if a + b <= c:
-        return("NotATriangle", False)
+        return("NotATriangle", "Nonright")
     elif a != b != c:
         triangleType = "Scalene"
     elif a == c:
@@ -16,7 +19,10 @@ def classify_triangle(a, b, c):
     else:
         triangleType = "Isosceles"
 
-    rightTriangle = sqrt(a**2 + b**2) == c or sqrt(a**2 + c**2) == b or sqrt(b**2 + c**2) == a
+    if sqrt(a**2 + b**2) == c or sqrt(a**2 + c**2) == b or sqrt(b**2 + c**2) == a:
+        rightTriangle = "Right"
+    else:
+        rightTriangle = "Nonright"
 
     return(triangleType, rightTriangle)
 
@@ -24,23 +30,31 @@ def classify_triangle(a, b, c):
 
 class Testclassify_triangle(unittest.TestCase):
     def test_nonright_scalene(self):
-        self.assertEqual(classify_triangle(1, 2.5, 3), ("Scalene", False))
+        self.assertEqual(classify_triangle(1, 2.5, 3), ("Scalene", "Nonright"))
 
     def test_right_scalene(self):
-        self.assertEqual(classify_triangle(3, 4, 5), ("Scalene", True))
+        self.assertEqual(classify_triangle(3, 4, 5), ("Scalene", "Right"))
 
     def test_nonright_isosceles(self):
-        self.assertEqual(classify_triangle(1, 1, 1.5), ("Isosceles", False))
+        self.assertEqual(classify_triangle(1, 1, 1.5), ("Isosceles", "Nonright"))
 
     def test_right_isosceles(self):
-        self.assertEqual(classify_triangle(1, 1, sqrt(2)), ("Isosceles", True))
+        self.assertEqual(classify_triangle(1, 1, sqrt(2)), ("Isosceles", "Right"))
 
     def test_equilateral(self):
-        self.assertEqual(classify_triangle(3, 3, 3), ("Equilateral", False))
+        self.assertEqual(classify_triangle(3, 3, 3), ("Equilateral", "Nonright"))
 
     def test_notATriangle(self):
-        self.assertEqual(classify_triangle(1, 1, 3), ("NotATriangle", False))
+        self.assertEqual(classify_triangle(1, 1, 3), ("NotATriangle", "Nonright"))
 
 
 if __name__ == '__main__':
-    unittest.main()
+    # show the classify_triangle function running as intended
+    print(classify_triangle(1,2,2.5))
+    print(classify_triangle(1,1,1))
+    print(classify_triangle(3, 3, 3))
+    print(classify_triangle(3, 4, 5))
+
+    print(classify_triangle(6, 7, 3))
+    # run the tests
+    unittest.main(exit=True)
