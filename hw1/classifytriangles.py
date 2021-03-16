df267e68454ac9d644256b9842816e77ffde3919
A-very-Cunning-ham <ave210knife@gmail.com>
2021-03-16 14:12:19 -0400

    Update linter

--- hw1/classifytriangles.py
+++ hw1/classifytriangles.py
@@ -1,32 +1,41 @@
+"""Module that helps to classify triangles"""
 from math import sqrt
 
 
 def classify_triangle(a, b, c):
-    """ 
-    Based on 3 side lengths, return the triangle properties: scalene, isosceles, or equilateral, and if it is a right triangle.
-    Triangles are invalid if the summed length of their two shortest sides is shorter than the longest side
+    """
+    Based on 3 side lengths, return the triangle properties: scalene, isosceles,
+     or equilateral, and if it is a right triangle.
+    Triangles are invalid if the summed length of their two shortest sides is
+     shorter than the longest side
     """
 
-    if not (isinstance(a, (int, float)) or isinstance(b, (int, float)) or isinstance(c, (int, float))) or a < 0 or b < 0 or c < 0:
-        return("InvalidInput", "Nonright")
+    if (
+        not (
+            isinstance(a, (int, float))
+            or isinstance(b, (int, float))
+            or isinstance(c, (int, float))
+        )
+        or a < 0
+        or b < 0
+        or c < 0
+    ):
+        return ("InvalidInput", "Nonright")
 
     a, b, c = sorted([a, b, c])
 
     if a + b <= c:
-        return("NotATriangle", "Nonright")
+        return ("NotATriangle", "Nonright")
     elif a != b != c:
-        triangleType = "Scalene"
+        triangle_type = "Scalene"
     elif a == c:
-        triangleType = "Equilateral"
+        triangle_type = "Equilateral"
     else:
-        triangleType = "Isosceles"
+        triangle_type = "Isosceles"
 
-    if sqrt(a**2 + b**2) == c:
-        rightTriangle = "Right"
+    if sqrt(a ** 2 + b ** 2) == c:
+        right_triangle = "Right"
     else:
-        rightTriangle = "Nonright"
+        right_triangle = "Nonright"
 
-    return(triangleType, rightTriangle)
-
-
-
+    return (triangle_type, right_triangle)
--- hw1/test_classifytriangles.py
+++ hw1/test_classifytriangles.py
@@ -1,8 +1,10 @@
 import unittest
-from classifytriangles import classify_triangle
 from math import sqrt
+from classifytriangles import classify_triangle
 
-class Testclassify_triangle(unittest.TestCase):
+
+class TestClassifyTriangle(unittest.TestCase):
+
     def test_nonright_scalene(self):
         self.assertEqual(classify_triangle(1, 2.5, 3), ("Scalene", "Nonright"))
 
@@ -18,24 +20,24 @@ class Testclassify_triangle(unittest.TestCase):
     def test_equilateral(self):
         self.assertEqual(classify_triangle(3, 3, 3), ("Equilateral", "Nonright"))
 
-    def test_notATriangle(self):
+    def test_notatriangle(self):
         self.assertEqual(classify_triangle(1, 1, 3), ("NotATriangle", "Nonright"))
-    
-    def test_invalidInput(self):
-        self.assertEqual(classify_triangle('a', 'b', 'c'), ("InvalidInput", "Nonright"))
 
-    def test_negativeInput(self):
+    def test_invalid_input(self):
+        self.assertEqual(classify_triangle("a", "b", "c"), ("InvalidInput", "Nonright"))
+
+    def test_negative_input(self):
         self.assertEqual(classify_triangle(-1, 2, 3), ("InvalidInput", "Nonright"))
 
-    def test_negativeInput2(self):
+    def test_negative_input2(self):
         self.assertEqual(classify_triangle(-2, -2, -3), ("InvalidInput", "Nonright"))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # show the classify_triangle function running as intended
-    print(classify_triangle(1,1,1.5))
-    print(classify_triangle(1,1,sqrt(2)))
-    print(classify_triangle(1,1,1))
+    print(classify_triangle(1, 1, 1.5))
+    print(classify_triangle(1, 1, sqrt(2)))
+    print(classify_triangle(1, 1, 1))
     print(classify_triangle(3, 3, 3))
     print(classify_triangle(3, 4, 5))
     print(classify_triangle(6, 7, 3))
